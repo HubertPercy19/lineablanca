323f444dc77e7fe8ac73d991d58403c3203dbcc0
Hubert <hubert@vicom.mx>
2025-11-30 23:27:20 -0500

    Fix Styles transitions

--- js/main.js
+++ js/main.js
@@ -16,11 +16,28 @@ const header = document.querySelector   ('header');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
+    console.log('scroll')
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
 });
+
+const elementos = document.querySelectorAll('.scroll-anim');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      // Opcional: dejar de observar para que no se repita
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.5 // porcentaje que debe entrar en pantalla
+});
+
+elementos.forEach(el => observer.observe(el));
 
 const track = document.querySelector(".slider-track");
 const cards = document.querySelectorAll(".section__preguntas__card");
