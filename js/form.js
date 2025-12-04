3daa762d382bd0cc4b1ae6fc0e6d0e7484b06389
Hubert <hubert@vicom.mx>
2025-12-03 22:28:37 -0500

    Add Form Frontend

---
+++ js/form.js
@@ -0,0 +1,7 @@
+export const sendForm = () => {
+    const buttonSubmit = document.querySelector('.submit');
+    buttonSubmit.addEventListener('click',(e) => {
+        e.preventDefault();
+        console.log('esperando....')
+    })
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { scrollAnimationHeader, scrollAnimationSection } from "./animation.js";
+import { sendForm } from "./form.js";
 import { activeNavBar } from "./nav-bar.js";
 import { initSlider } from "./slider.js";
 
@@ -7,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollAnimationSection()
     activeNavBar()
     initSlider()
+    sendForm()
     // Aquí ya puedes acceder a tus elementos del HTML
 });
 
